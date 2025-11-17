ed69dd45065d5e7978e15687d31cd8fe2566fdf1
Tom Berthoud <tom.berthoud@hes-so.ch>
2025-11-17 14:37:46 +0100

    open latex in chrome

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -3,10 +3,12 @@ return {
     "lervag/vimtex",
     lazy = false,
     init = function()
-      vim.g.vimtex_view_method = "zathura" -- Ouvre automatiquement le PDF avec Zathura
-      vim.g.vimtex_compiler_method = "latexmk" -- Compilation automatique
-      vim.g.vimtex_quickfix_mode = 0 -- Pas de fenêtre d’erreur automatique
-      vim.g.vimtex_complete_enabled = 1 -- Active la complétion LaTeX
+      vim.g.vimtex_view_method = "general" -- View method générique
+      vim.g.vimtex_view_general_viewer = "wslview" -- Ouvre PDF dans Chrome Windows
+
+      vim.g.vimtex_compiler_method = "latexmk"
+      vim.g.vimtex_quickfix_mode = 0
+      vim.g.vimtex_complete_enabled = 1
       vim.g.tex_flavor = "latex"
     end,
   },
